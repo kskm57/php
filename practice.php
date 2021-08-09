ba4bde6e8d393f68ae80cfadaa6c0465da21d142
河田将太 <shota.kawata1104@gmail.com>
2021-08-09 15:39:54 +0000

    rev2

--- practice.php
+++ practice.php
@@ -1,32 +1,32 @@
 <?php
 echo  'hello php';
 
-echo '<br>';
+echo "\n";
 
 $a = 3;
 $b = 7;
 echo '課題1' . $a + $b;
 
-echo '<br>';
+echo "\n";
 
 $array_month = ["1月", "2月", "3月", "4月", "5月", "6月", 
 "7月", "8月", "9月", "10月", "11月", "12月"];
 echo '課題2 ' . $array_month[7];
 
-echo '<br>';
+echo "\n";
 
 $hello = "Hello, ";
 $name = "Shota";
 $world = "'s World!";
 echo '課題3 ' . $hello.$name.$world;
 
-echo '<br>';
+echo "\n";
 
 $tech_boost = "tech ";
 $tech_boost .="boost";
 echo '課題4 ' . $tech_boost;
 
-echo '<br>';
+echo "\n";
 
 $calendar_2018 = [
   "January" => "1月",
